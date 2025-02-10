3fccacd93b57583ea497f53de8b3cc60104b506a
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2025-02-10 16:32:19 +0100

    Lock autofocus instead of setting to the nearest focus point

--- libpretixui-android/src/main/java/eu/pretix/libpretixui/android/scanning/ScannerView.kt
+++ libpretixui-android/src/main/java/eu/pretix/libpretixui/android/scanning/ScannerView.kt
@@ -126,20 +126,13 @@ class ScannerView : FrameLayout {
                     )
                     .build()
         } else {
-            val chars = Camera2CameraInfo.from(camera!!.cameraInfo)
-            val builder = CaptureRequestOptions.Builder()
-            builder.setCaptureRequestOption(
-                CaptureRequest.CONTROL_AF_MODE,
-                CameraMetadata.CONTROL_AF_MODE_OFF
-            )
-            val distance = chars.getCameraCharacteristic(CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE)
-            if (distance != null) {
-                builder.setCaptureRequestOption(
-                    CaptureRequest.LENS_FOCUS_DISTANCE,
-                    distance
-                )
-            }
-            Camera2CameraControl.from(camera!!.cameraControl).captureRequestOptions = builder.build()
+            Camera2CameraControl.from(camera!!.cameraControl).captureRequestOptions =
+                CaptureRequestOptions.Builder()
+                    .setCaptureRequestOption(
+                        CaptureRequest.CONTROL_AF_MODE,
+                        CameraMetadata.CONTROL_AF_MODE_AUTO
+                    )
+                    .build()
         }
         autofocusState = enabled
     }
